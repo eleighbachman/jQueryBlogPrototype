5a746f74a8db5e66f978d3e543f509fdec46053f
Liz Bachman <eleighbachman@gmail.com>
2018-03-31 10:19:47 -0600

    Final edits to blog post page

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ $('#flashMessage').hide();
 
 
 
+
+
+
 $('#previewButton').on('click', () => {
 
     $postTitle = $('#blogTitle').val();
@@ -35,11 +38,19 @@ $('#previewButton').on('click', () => {
 })
 
 $('#submitButton').on('click', () => {
-  $('#blogTitle').val('');
-  $('#blogContent').val('');
-  let d = new Date();
-  $dateTime.html("Changes saved at: " + d);
-  $('#flashMessage').html('Your changes have been saved!');
-  $('#flashMessage').slideDown(1000).delay(2000).slideUp(1000);
 
+    $('#blogTitle').val('');
+    $('#blogContent').val('');
+    let d = new Date();
+    $dateTime.html("Changes saved at: " + d);
+    $('#flashMessage').html('Your changes have been saved!');
+
+    $('#flashMessage').slideDown(1000).delay(2000).slideUp(1000);
+
+
+})
+
+$('#editButton').on('click', ()=> {
+  $('#blogTitle').val(($('#newTitle').html()));
+  $('#blogContent').val(($('#newEntry').html()));
 })
